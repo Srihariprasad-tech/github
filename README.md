1ec7abf622486f9b3f3c7eeaef0c6c4bdc9a8f45
srihari <srihariprasads91@gmail.com>
2026-07-05 12:25:33 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # this is srihariprasad s 
  # you can know more about me in linkedin and github
+ 
--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
 const a =2;
 console.log(a);
+const b,c;
+b=10;
+c=100;
+const d=a*b*c;
+console.log(d);
